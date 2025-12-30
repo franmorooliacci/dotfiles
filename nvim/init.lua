c3a802d616f6b7a799341cf445a981e5152a4ec3
franmorooliacci <franmorooliacci@gmail.com>
2025-12-30 12:09:24 -0300

    feat: add folding support

--- nvim/init.lua
+++ nvim/init.lua
@@ -92,6 +92,25 @@ require('lazy').setup({
         },
     },
 
+    -- Folding
+    {
+        'kevinhwang91/nvim-ufo',
+        dependencies = { 'kevinhwang91/promise-async' },
+        event = 'BufReadPost',
+        config = function()
+            vim.o.foldcolumn = '0'
+            vim.o.foldlevel = 99
+            vim.o.foldlevelstart = 99
+            vim.o.foldenable = true
+
+            require('ufo').setup({
+                provider_selector = function(_, _, _)
+                    return { 'lsp', 'treesitter' }
+                end,
+            })
+        end,
+    },
+
     --------------------------------------------------------
     -- Treesitter (SAFE)
     --------------------------------------------------------
